c427213a87ea010d12d6bb903a71161219af2b3a
Marko Vasilic <vasilicsingli@gmail.com>
2018-07-18 21:41:30 +0200

    AliExpress
    Testing the functionality of a site

---
+++ AliExpress/Search.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AliExpress
+{
+    [TestFixture]
+    [Order(2)]
+    class Search : ChromeOpen
+    {
+        public string productName = "Hindfield Designer Sunglasses Women Lunette De Soleil";
+        //sa ovim je lako BINYEAE blue mirror sunglases women cat eye UV400
+        //Hindfield Designer Sunglasses Women Lunette De Soleil
+
+        [Test]
+        [Order(2)]
+        public void SearchProduct()
+        {
+            Thread.Sleep(4000);
+            IWebElement searchTextBox = browser.FindElement(By.Id("search-key"));
+            searchTextBox.SendKeys(productName);
+            IWebElement searchButton = browser.FindElement(By.CssSelector("input.search-button"));
+            searchButton.Click();
+
+
+            IWebElement result = browser.FindElement(By.Id("hs-below-list-items"));
+            //niz
+            IWebElement[] resultArray = result.FindElements(By.TagName("li")).ToArray();
+
+            for (int i = 0; i < resultArray.Count(); i++)
+            {
+                IWebElement naslov = resultArray[i].FindElement(By.CssSelector("div div:nth-child(2) h3 a"));
+
+                if (productName.Contains(naslov.Text))
+                {
+                    naslov.Click();
+                    Assert.Pass("Search works");
+                    break;
+                }
+                else
+                {
+                    Assert.Fail("Search does not work");
+                }
+            }
+        }
+
+        [Test]
+        [Order(3)]
+        public void EnterProduct()
+        {
+            Thread.Sleep(4000);
+            browser.SwitchTo().Window(browser.WindowHandles.Last());
+            IWebElement Color = browser.FindElement(By.Id("sku-1-10"));
+            Color.Click();
+            /*IWebElement Quantity = browser.FindElement(By.Id("j-p-quantity-input"));
+            Quantity.Clear();
+            Quantity.SendKeys("5");*/
+            IWebElement AddToCart = browser.FindElement(By.Id("j-add-cart-btn"));
+            AddToCart.Click();
+            Thread.Sleep(2000);
+            IWebElement ViewCart = browser.FindElement(By.LinkText("View Shopping Cart"));
+            ViewCart.Click();
+
+            Assert.Pass("You have successfully purchased the product");
+            
+
+
+        }
+    }
+}
---
+++ AliExpress/TestLogin.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AliExpress
+{
+    [TestFixture]
+    [Order(1)]
+    class TestLogin : ChromeOpen
+    {
+        public TestLogin()
+        {
+            browser.Manage().Window.Maximize();
+            browser.Url = "https://www.aliexpress.com/";
+
+        }
+
+        [Test]
+        [Order(1)]
+        public void Login()
+        {
+            IWebElement signIn = browser.FindElement(By.LinkText("Sign in"));
+            signIn.Click();
+            Thread.Sleep(2000);
+            browser.SwitchTo().Frame("alibaba-login-box");
+            
+            IWebElement enterEmail = browser.FindElement(By.Id("fm-login-id"));
+            enterEmail.SendKeys(data.Email);
+            
+            IWebElement enterPass = browser.FindElement(By.Id("fm-login-password"));
+            enterPass.SendKeys(data.Password);
+            IWebElement Submit = browser.FindElement(By.Id("fm-login-submit"));
+            Submit.Click();
+
+            Assert.Pass("User logged in");
+        }
+    }
+}
